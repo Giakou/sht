2b8a0a6795f4a628bef15413701f431e4d4a1829
Georgios Giakoustidis <georgios.giakoustidis@desy.de>
2023-04-24 02:49:35 +0900

    MAINT: Move sht85 and sht2x driver files to the respective directories

---
+++ sht85/test_run_sht85.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Test script for basic SHT85 functionality
+"""
+
+import sht85
+import time
+
+if __name__ == '__main__':
+    # Create SHT85 object
+    mysensor = sht85.SHT85(bus=1, mps=1, rep='high')
+
+    # Check S/N
+    print('serial number = ', mysensor.sn)
+
+    try:
+        while True:
+            # Single shot mode is preferred due to less current consumption (x8-x200) in idle state
+            mysensor.single_shot()
+            print(f'Temperature = {mysensor.t} deg')
+            print(f'Relative Humidity = {mysensor.rh}%')
+            print(f'Dew Point = {mysensor.dp} degrees')
+            time.sleep(mysensor.mps)
+
+    except (KeyboardInterrupt, SystemExit):
+        print("Killing Thread...")
+    finally:
+        mysensor.stop()
